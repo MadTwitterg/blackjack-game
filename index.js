b144960c89491a248bb5e9d9c5a93ddf82d18839
MadTwitterg <sakshumwalia43026@gmail.com>
2026-04-03 19:21:14 +0530

    messages are displayed in para text

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 //define variables
-let firstCard = 10
+let firstCard = 9
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
 
 //check the cards
 function startGame() {
     if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
+    message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo, you've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackjack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
 
-    console.log(message)
+    messageEl.textContent = message
 }
