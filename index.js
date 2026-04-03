c1493e2066d85562a948c8c7c9aa17927e5fc8c5
MadTwitterg <sakshumwalia43026@gmail.com>
2026-04-03 18:10:58 +0530

    basically nothing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let firstCard = 11
-let secondCard = 11
+let firstCard = 10
+let secondCard = 9
 
 sum = firstCard + secondCard
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo, you've got Blackjack! 🥳")
 } else {
-    console.log("You're lost the game! 😭")
+    console.log("You're out of the game! 😭")
 }
