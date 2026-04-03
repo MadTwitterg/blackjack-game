cfdb8219176ae396268dcea923f0125b74f3452a
MadTwitterg <sakshumwalia43026@gmail.com>
2026-04-03 17:52:41 +0530

    added if...else conditionals

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let firstCard = 4
-let secondCard = 9
+let firstCard = 11
+let secondCard = 11
 
 sum = firstCard + secondCard
 
-console.log(sum)
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo, you've got Blackjack! 🥳")
+} else {
+    console.log("You're lost the game! 😭")
+}
